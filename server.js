f52a29e5fe352e8af54e80c55299c95b08a695ea
raghuveer suthar <ayushsuthar1234567@gmail.com>
2026-04-29 18:30:23 +0530

    Update fallback port to 3000

--- server.js
+++ server.js
@@ -37,7 +37,7 @@ app.get('/', (req, res) => {
   res.send('API is running...');
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`Server running in mode on port ${PORT}`);
